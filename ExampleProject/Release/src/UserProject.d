de2c9c5fafb91c799d7bb8260417c7f86e31ce6d
Scott Rowan <s.rowan@hud.ac.uk>
2017-05-11 10:34:56 +0100

    Initial Commit

---
+++ ExampleProject/Release/src/UserProject.d
@@ -0,0 +1,70 @@
+src/UserProject.o: ../src/UserProject.cpp \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/AcceleratorComponent.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/merlin_config.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/EMField.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Space3D.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/VectorTags.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/utils.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/AcceleratorGeometry.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Transform3D.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Transform2D.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Space2D.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Rotation3D.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/LinearAlgebra.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/TLAS.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/TMatrixLib.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/tblas.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Complex.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/RotationType.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Rot3Drep.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Aperture.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/Material.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/ModelElement.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/WakePotentials.h \
+ /home/scott/git/merlin-eclipse/MerlinLibrary/BunchProcess.h
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/AcceleratorComponent.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/merlin_config.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/EMField.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Space3D.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/VectorTags.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/utils.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/AcceleratorGeometry.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Transform3D.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Transform2D.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Space2D.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Rotation3D.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/LinearAlgebra.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/TLAS.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/TMatrixLib.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/tblas.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Complex.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/RotationType.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Rot3Drep.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Aperture.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/Material.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/ModelElement.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/WakePotentials.h:
+
+/home/scott/git/merlin-eclipse/MerlinLibrary/BunchProcess.h:
