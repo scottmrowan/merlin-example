81efd9b8e6e5a056957d2536586aa5fef5598eb9
Scott Rowan <s.rowan@hud.ac.uk>
2017-06-28 11:35:17 +0100

    Fixed .cproject library config

--- ExampleProject1/Release/src/subdir.mk
+++ ExampleProject1/Release/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/lhc_fft_tune_test.cpp 
+../src/cu50_test.cpp 
 
 OBJS += \
-./src/lhc_fft_tune_test.o 
+./src/cu50_test.o 
 
 CPP_DEPS += \
-./src/lhc_fft_tune_test.d 
+./src/cu50_test.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++1y -I"/home/scott/git/merlin-eclipse/MerlinLibrary" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
